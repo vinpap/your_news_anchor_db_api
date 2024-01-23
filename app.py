37ed92fd60acad0a88153d97450a4832e9394179
Vincent Papelard <vincent.papelard@logiroad-center.com>
2024-01-23 14:41:36 +0100

    Now it works

--- app.py
+++ app.py
@@ -26,9 +26,6 @@ with open("./db_pwd.txt") as pwd_file:
     config["database_password"] = pwd_file.read()
 
 
-# À SUPPRIMER !
-print(config)
-
 # Inits the database
 conn = psycopg2.connect(database=config["database_name"],
                     host=config["database_url"],
