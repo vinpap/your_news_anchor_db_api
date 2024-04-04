91ccd0f124b47eaee8e2e8af39623dda36d35f01
Vincent <papelardvincent@gmail.com>
2024-04-04 11:37:47 +0200

    Added the /all_feeds endpoint

--- app.py
+++ app.py
@@ -79,7 +79,10 @@ async def root():
 @app.post("/feeds", status_code=200)
 async def get_rss_feeds(retrieval_request: FeedsRetrievalRequest, response: Response):
     """
-    Returns a list of all the RSS feeds to extract data from.
+    Returns a list of all the RSS feeds to extract data from available to a specific user.
+
+    This includes all standard RSS feeds (available to everyone) as well as
+    the RSS feeds the user registered himself.
     """
     if retrieval_request.security_token != config["security_token"]:
         return JSONResponse(status_code=401, content={"msg": "Your security code is not valid"})
@@ -99,9 +102,35 @@ async def get_rss_feeds(retrieval_request: FeedsRetrievalRequest, response: Resp
         JSONResponse(status_code=401, content={"msg": f"Error: could not retrieve id for use {username}"})
     user_id = results[0][0]
     print(user_id)
-    fetch_rss_feeds_request = f"""SELECT id, url, language, topic, name FROM (select id, url, language, topic, name from user_sources where user_id={user_id}) union  (select id, url, language, topic, name from standard_sources where not exists (select * from user_sources where user_sources.url=standard_sources.url));"""
+    fetch_rss_feeds_request = f"""SELECT url, language, topic, name FROM (select url, language, topic, name from user_sources where user_id={user_id}) union  (select url, language, topic, name from standard_sources where not exists (select * from user_sources where user_sources.url=standard_sources.url));"""
     
     cursor.execute(fetch_rss_feeds_request)
+    results = cursor.fetchall()
+
+    # Turning the resulting data into a dictionary for ease of use
+    dict_results = []
+    for rss in results:
+        dict_rss = {
+            "name": rss[3],
+            "url": rss[0],
+            "topic": rss[2],
+            "language": rss[1]
+        }
+        dict_results.append(dict_rss)
+    return dict_results
+
+@app.post("/all_feeds", status_code=200)
+async def get_all_rss_feeds(secret_token: str, response: Response):
+    """
+    Returns a list of all the RSS feeds saved in the database.
+    """
+    if secret_token != config["security_token"]:
+        return JSONResponse(status_code=401, content={"msg": "Your security code is not valid"})
+    
+    # Retrieving user-added sources
+    fetch_user_rss_feeds_request = f"""SELECT id, url, language, topic, name FROM user_sources;"""
+    cursor = conn.cursor()
+    cursor.execute(fetch_user_rss_feeds_request)
     results = cursor.fetchall()
 
     # Turning the resulting data into a dictionary for ease of use
@@ -112,10 +141,27 @@ async def get_rss_feeds(retrieval_request: FeedsRetrievalRequest, response: Resp
             "name": rss[4],
             "url": rss[1],
             "topic": rss[3],
-            "language": rss[2]
+            "language": rss[2],
+            "is_from_user": True
+        }
+        dict_results.append(dict_rss)
+    
+    # Retrieving standard sources
+    fetch_standard_rss_feeds_request = f"""SELECT id, url, language, topic, name from standard_sources;"""
+    cursor.execute(fetch_standard_rss_feeds_request)
+    results = cursor.fetchall()
+    for rss in results:
+        dict_rss = {
+            "source_id": rss[0],
+            "name": rss[4],
+            "url": rss[1],
+            "topic": rss[3],
+            "language": rss[2],
+            "is_from_user": False
         }
         dict_results.append(dict_rss)
     return dict_results
+
 
 
 @app.post("/update_articles", status_code=200)
