357ea4c80c40413f820423e428ebc757ee21ea3d
Vincent <papelardvincent@gmail.com>
2024-04-04 14:17:27 +0200

    Finished debugging the API

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ database itself is implemented using PostgreSQL.
 In order to run this script, execute uvicorn app:app --reload in the directory 
 where this script is located.
 """
+
 import os
 from typing import List
 from datetime import datetime
@@ -27,17 +28,20 @@ config["database_password"] = os.environ["DB_PWD"]
 
 
 # Inits the database
-conn = psycopg2.connect(database=config["database_name"],
-                    host=config["database_url"],
-                    user=config["database_user"],
-                    password=config["database_password"],
-                    port=config["database_port"])
+conn = psycopg2.connect(
+    database=config["database_name"],
+    host=config["database_url"],
+    user=config["database_user"],
+    password=config["database_password"],
+    port=config["database_port"],
+)
 
 
 class Article(BaseModel):
     """
     Data structure that stores information about an article.
     """
+
     source_id: int
     source: str
     url: str
@@ -53,21 +57,25 @@ class Article(BaseModel):
     date: str
     is_from_user: bool
 
+
 class ArticlesUpdateRequest(BaseModel):
     """
-    Data sent to the /update_articles endpoint to request an update of the 
+    Data sent to the /update_articles endpoint to request an update of the
     daily articles stored in the database.
     """
+
     security_token: str
     articles: List[Article]
+
 
 class FeedsRetrievalRequest(BaseModel):
     """
     Request sent to the /feeds in order to retrieve the list of feeds for
     a user.
     """
-    username: str # The name of the user
-    security_token: str # The API security token
+
+    username: str  # The name of the user
+    security_token: str  # The API security token
 
 
 @app.get("/")
@@ -76,6 +84,7 @@ async def root():
     Returns a help message.
     """
     return {"msg": "Please send requests to the /feeds and /update_articles endpoints"}
+
 
 @app.post("/feeds", status_code=200)
 async def get_rss_feeds(retrieval_request: FeedsRetrievalRequest, response: Response):
@@ -86,39 +95,43 @@ async def get_rss_feeds(retrieval_request: FeedsRetrievalRequest, response: Resp
     the RSS feeds the user registered himself.
     """
     if retrieval_request.security_token != config["security_token"]:
-        return JSONResponse(status_code=401, content={"msg": "Your security code is not valid"})
-    
+        return JSONResponse(
+            status_code=401, content={"msg": "Your security code is not valid"}
+        )
+
     username = retrieval_request.username
     user_exists_request = f"""SELECT * FROM users WHERE username='{username}'"""
     cursor = conn.cursor()
     cursor.execute(user_exists_request)
     results = cursor.fetchall()
     if not results:
-        return JSONResponse(status_code=401, content={"msg": f"User {username} does not exist in the database"})
+        return JSONResponse(
+            status_code=401,
+            content={"msg": f"User {username} does not exist in the database"},
+        )
 
     get_id_query = f"""SELECT id from users where username='{username}'"""
     cursor.execute(get_id_query)
     results = cursor.fetchall()
     if not results:
-        JSONResponse(status_code=401, content={"msg": f"Error: could not retrieve id for use {username}"})
+        JSONResponse(
+            status_code=401,
+            content={"msg": f"Error: could not retrieve id for use {username}"},
+        )
     user_id = results[0][0]
     print(user_id)
     fetch_rss_feeds_request = f"""SELECT url, language, topic, name FROM (select url, language, topic, name from user_sources where user_id={user_id}) union  (select url, language, topic, name from standard_sources where not exists (select * from user_sources where user_sources.url=standard_sources.url));"""
-    
+
     cursor.execute(fetch_rss_feeds_request)
     results = cursor.fetchall()
 
     # Turning the resulting data into a dictionary for ease of use
     dict_results = []
     for rss in results:
-        dict_rss = {
-            "name": rss[3],
-            "url": rss[0],
-            "topic": rss[2],
-            "language": rss[1]
-        }
+        dict_rss = {"name": rss[3], "url": rss[0], "topic": rss[2], "language": rss[1]}
         dict_results.append(dict_rss)
     return dict_results
+
 
 @app.post("/all_feeds", status_code=200)
 async def get_all_rss_feeds(secret_token: str, response: Response):
@@ -126,10 +139,14 @@ async def get_all_rss_feeds(secret_token: str, response: Response):
     Returns a list of all the RSS feeds saved in the database.
     """
     if secret_token != config["security_token"]:
-        return JSONResponse(status_code=401, content={"msg": "Your security code is not valid"})
-    
+        return JSONResponse(
+            status_code=401, content={"msg": "Your security code is not valid"}
+        )
+
     # Retrieving user-added sources
-    fetch_user_rss_feeds_request = f"""SELECT id, url, language, topic, name FROM user_sources;"""
+    fetch_user_rss_feeds_request = (
+        f"""SELECT id, url, language, topic, name FROM user_sources;"""
+    )
     cursor = conn.cursor()
     cursor.execute(fetch_user_rss_feeds_request)
     results = cursor.fetchall()
@@ -143,12 +160,14 @@ async def get_all_rss_feeds(secret_token: str, response: Response):
             "url": rss[1],
             "topic": rss[3],
             "language": rss[2],
-            "is_from_user": True
+            "is_from_user": True,
         }
         dict_results.append(dict_rss)
-    
+
     # Retrieving standard sources
-    fetch_standard_rss_feeds_request = f"""SELECT id, url, language, topic, name from standard_sources;"""
+    fetch_standard_rss_feeds_request = (
+        f"""SELECT id, url, language, topic, name from standard_sources;"""
+    )
     cursor.execute(fetch_standard_rss_feeds_request)
     results = cursor.fetchall()
     for rss in results:
@@ -158,11 +177,10 @@ async def get_all_rss_feeds(secret_token: str, response: Response):
             "url": rss[1],
             "topic": rss[3],
             "language": rss[2],
-            "is_from_user": False
+            "is_from_user": False,
         }
         dict_results.append(dict_rss)
     return dict_results
-
 
 
 @app.post("/update_articles", status_code=200)
@@ -172,38 +190,38 @@ async def update_articles(new_articles: ArticlesUpdateRequest, response: Respons
     replaces them with the ones sent through this request.
     """
 
-
     # Checking the security token to make sure the user is allowed to update
     # the daily articles, as this is a destructive action.
     # Any unauthorized request will yield a 401 error.
     if new_articles.security_token != config["security_token"]:
-        return JSONResponse(status_code=401, content={"msg": "Your security code is not valid"})
-    
+        return JSONResponse(
+            status_code=401, content={"msg": "Your security code is not valid"}
+        )
+
     articles = new_articles.articles
     # Adding the new articles to the daily_articles table
     cursor = conn.cursor()
-    cursor.execute("SELECT id, is_from_user FROM daily_articles")
+    cursor.execute("SELECT id FROM daily_articles")
     old_article_id_tuples = cursor.fetchall()
     old_article_ids = []
 
     for id_tuple in old_article_id_tuples:
-        old_article_ids.append(str(id_tuple))
+        old_article_ids.append(str(id_tuple[0]))
     for article in articles:
 
         # Formatting the date (if any) before inserting it in the table
         date = article.date
-        if date: # i.e. if the date string is not empty
-            date = datetime.strptime(date, '%a %d %b %Y, %I:%M%p')
+        if date:  # i.e. if the date string is not empty
+            date = datetime.strptime(date, "%a %d %b %Y, %I:%M%p")
             if article.is_from_user:
-                request = f"""INSERT INTO daily_articles(title, url, content, authors, date, image, is_from_user, user_source_id) VALUES('{article.title.replace("'", '"')}', '{article.url.replace("'", '"')}', '{article.content.replace("'", '"')}', '{article.authors.replace("'", '"')}', '{date}', '{article.image}', true, {article.source_id})"""
+                request = f"""INSERT INTO daily_articles(title, url, content, author, date, image, is_from_user, user_source_id) VALUES('{article.title.replace("'", '"')}', '{article.url.replace("'", '"')}', '{article.content.replace("'", '"')}', '{article.authors.replace("'", '"')}', '{date}', '{article.image}', true, {article.source_id})"""
             else:
-                request = f"""INSERT INTO daily_articles(title, url, content, authors, date, image, is_from_user, standard_source_id) VALUES('{article.title.replace("'", '"')}', '{article.url.replace("'", '"')}', '{article.content.replace("'", '"')}', '{article.authors.replace("'", '"')}', '{date}', '{article.image}', false, {article.source_id})"""
+                request = f"""INSERT INTO daily_articles(title, url, content, author, date, image, is_from_user, standard_source_id) VALUES('{article.title.replace("'", '"')}', '{article.url.replace("'", '"')}', '{article.content.replace("'", '"')}', '{article.authors.replace("'", '"')}', '{date}', '{article.image}', false, {article.source_id})"""
         else:
             if article.is_from_user:
-                request = f"""INSERT INTO daily_articles(title, url, content, authors, image, is_from_user, user_source_id) VALUES('{article.title.replace("'", '"')}', '{article.url.replace("'", '"')}', '{article.content.replace("'", '"')}', '{article.authors.replace("'", '"')}', '{article.image}', true, {article.source_id})"""
+                request = f"""INSERT INTO daily_articles(title, url, content, author, image, is_from_user, user_source_id) VALUES('{article.title.replace("'", '"')}', '{article.url.replace("'", '"')}', '{article.content.replace("'", '"')}', '{article.authors.replace("'", '"')}', '{article.image}', true, {article.source_id})"""
             else:
-                request = f"""INSERT INTO daily_articles(title, url, content, authors, image, is_from_user, standard_source_id) VALUES('{article.title.replace("'", '"')}', '{article.url.replace("'", '"')}', '{article.content.replace("'", '"')}', '{article.authors.replace("'", '"')}', '{article.image}', false, {article.source_id})"""
-        return request
+                request = f"""INSERT INTO daily_articles(title, url, content, author, image, is_from_user, standard_source_id) VALUES('{article.title.replace("'", '"')}', '{article.url.replace("'", '"')}', '{article.content.replace("'", '"')}', '{article.authors.replace("'", '"')}', '{article.image}', false, {article.source_id})"""
         cursor.execute(request)
     conn.commit()
 
@@ -213,15 +231,5 @@ async def update_articles(new_articles: ArticlesUpdateRequest, response: Respons
     # the table, thus leading to an empty table.
     if old_article_ids:
         request = f"""DELETE FROM daily_articles WHERE id IN ({", ". join(old_article_ids)});"""
-    else:
-        request = f"""DELETE * FROM daily_articles;"""
-    cursor.execute(request)
-    conn.commit()
-
-
-    
-
-
-
-
-
+        cursor.execute(request)
+        conn.commit()
