5c0e22dd75ba5045deccf75f31ba892bdfd515f7
Vincent Papelard <papelardvincent@gmail.com>
2024-04-24 11:26:34 +0200

    :white_check_mark: Create test_api.py

---
+++ test_api.py
@@ -0,0 +1,17 @@
+"""
+This file contains tests designed to make sure the API works properly.
+"""
+
+def test_security():
+  """
+  Makes sure the endpoints are only available if the user has a valid token.
+  """
+  pass
+
+def test_get_rss():
+  """
+  Checks that the 'feeds' and 'all_feeds' endpoints return the right data
+  format (dict with feeds information)
+  """
+  pass
+  
